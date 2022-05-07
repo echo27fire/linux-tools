aec3e75674a640a62c6bf6f8b6e3b56315c273dd
echo27fire <echo27fire@gmail.com>
2022-05-07 02:36:00 +0000

    samba tool mod -beta

--- tools/samba-setup.sh
+++ tools/samba-setup.sh
@@ -58,11 +58,21 @@ echo "making SAMBA directory"
 sudo mkdir -p $smbdir || echo "error with directory creation"
 echo "installing samba and samba tools"
 sudo apt install samba cifs-utils || echo "error installing packages"
-clear;echo "samba install complete, making bakup of smb.conf"
+echo "samba install complete, making bakup of smb.conf"
 
 # stage 3 - backup inital config & make custom config
 smb-backup # config cp to user home dir.
 sudo chmod 664 -R /$smbdir
+sudo echo ' ' > ~/smb.conf # blanks out existing custom conf file if it exists change to if statement?
 CONF-MAKE
 #sudo cat ~/smb.conf > /etc/samba/smb.conf
 sudo cat  ~/smb.conf | sudo tee /etc/samba/smb.conf
+
+# stage 4 secure smb share
+sudo groupadd samba
+sudo usermod -aG samba $USER
+sudo useradd noc && sudo usermod -G samba noc
+sudo passwd -d noc
+mkdir /samba/influx;sudo chown noc:samba -R /samba/influx && sudo chmod 2770 /samba/influx
+
+
