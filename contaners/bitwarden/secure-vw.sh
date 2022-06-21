ac8793f7ba778f0aaf0c4b2a6489062697105210
echo27fire <echo27fire@gmail.com>
2022-06-20 23:18:08 -0400

    same

--- contaners/bitwarden/secure-vw.sh
+++ contaners/bitwarden/secure-vw.sh
@@ -6,10 +6,9 @@ echo $KEY
 docker run -d --name vaultwarden \
 --restart=always \
 -v /docker/appdata/vaultwarden:/data/ \
--p  85:443 \
--p 3012:3012 \
--e WEBSOCKET_ENABLED=true \
+-p  81:80 -e WEBSOCKET_ENABLED=true -p 3012:3012 \
 -e ADMIN_TOKEN="$KEY" \
 -e SIGNUPS_ALLOWED=false \
 vaultwarden/server:latest
-echo 'run complete'
+
+docker ps -a | grep vaultwarden
