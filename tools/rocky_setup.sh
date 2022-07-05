d320b7e305cde239260f3ca601690e0881bb33f1
echo27fire <echo27fire@gmail.com>
2022-07-04 22:03:13 -0400

    update to rocky

--- tools/rocky_setup.sh
+++ tools/rocky_setup.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+# variables
+CHECKMK='https://download.checkmk.com/checkmk/2.1.0p4/check-mk-raw-2.1.0p4-el8-38.x86_64.rpm'
+DIR=checkmk-tmp
+
+# repo installs
 dnf install epel-release -y
 dnf install elrepo-release -y
 dnf install rpmfusion-free-release -y
-
 dnf update -y
+dnf makecache
 
 echo 'repo and update process complete'
 
 dnf install cockpit -y
-systemctl start cockpit.socket
-systemctl enable cockpit.socket
+systemctl enable --now cockpit.socket
 
 echo 'cockpit configured'
 
 systemctl disable --now firewalld.service
+dnf install ufw -y
 systemctl enable --now ufw.service
 ufw disable
 ufw allow http
@@ -24,3 +29,13 @@ ufw allow 9090
 ufw enable
 
 echo 'ufw enabbled'
+
+# install checkmk
+
+cd /tmp 
+mkdir ./$DIR
+cd $DIR
+sudo setsebool -P httpd_can_network_connect on
+sudo dnf --enablerepo=powertools install graphviz-gd -y
+wget $CHECKMK
+sudo dnf install ./$CHECKMK 2>/tmp/$DIR/error
